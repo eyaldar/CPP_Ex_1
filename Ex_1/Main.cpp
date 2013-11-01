214f7e652b67b73c3743d3ccf852ec0063cabb04
eyaldar <eyaldar@gmail.com>
2013-11-01 16:31:20 +0200

    Added main function and gotoxy cabapillities

---
+++ Ex_1/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include "Gotoxy.h"
+
+using namespace std;
+
+void main()
+{
+	system("cls");
+	
+	_flushall();
+
+	int x=1;
+	int y=1;
+
+	// esc (Ascii 27) ends the loop
+	while(!_kbhit() || _getch()!=27)
+	{
+		gotoxy(x,y);
+		cout << "*" << endl;
+		Sleep(50);
+		gotoxy(x,y);
+		cout << " " << endl;
+		++x;
+		if(x>79) {x=1;}
+		++y;
+		if(y>20) {y=1;}
+	}
+
+}
